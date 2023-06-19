75138149ea744749dc51660fb60ef7badf272c63
Alie <alieneon22@gmail.com>
2023-06-19 11:14:41 +0200

    ej 2 types

---
+++ JS/Ejercicio02/index.js
@@ -0,0 +1,24 @@
+// * Data types
+
+let types = {
+  num: 1,
+  bool: true,
+  str: "queso",
+  none: null,
+  undef: undefined,
+  sym: Symbol(1),
+  arr: [],
+  obj: {},
+  fun: function(){}
+}
+
+function printDataType(content){
+  console.log("Data:", content, "Type:", typeof content)
+}
+
+for (const key in types) {
+  if (Object.hasOwnProperty.call(types, key)) {
+    const element = types[key];
+    printDataType(element)
+  }
+}
--- JS/index.md
+++ JS/index.md
@@ -3,3 +3,4 @@ Ejercicios de Javasript
 
 ## Contenidos 
 - [x] [Ejercicio 1: Prompts y alerts](Ejercicio01)
+- [x] [Ejercicio 2: Types](Ejercicio02)
