bb4ed9a0f340faf6055402e049dc6d6c1bdb0c42
Alie <alieneon22@gmail.com>
2023-06-21 10:36:17 +0200

    made ej 5 funtional

--- JS/Ejercicio05/index.js
+++ JS/Ejercicio05/index.js
@@ -1,29 +1,17 @@
 function sum(...nums) {
-  let acc = 0;
-  nums.forEach((num) => {
-    acc += num;
-  });
-  return acc;
+  return nums.reduce((acc, i) => acc + i);
 }
 
 console.log(sum(1, 2, 4));
 
 function concat(...strs) {
-  let acc = "";
-  strs.forEach((str) => {
-    acc += str + " ";
-  });
-  return acc.trim();
+  return strs.reduce((acc, i) => acc + " " + i);
 }
 
 console.log(concat("Alie", "Cejudo"));
 
 function max(...nums) {
-  let acc = -Infinity;
-  nums.forEach((num) => {
-    acc = num > acc ? num : acc;
-  });
-  return acc;
+  return nums.reduce((acc, i) => (acc > i ? acc : i));
 }
 
 console.log(max(3, 5, 2));
